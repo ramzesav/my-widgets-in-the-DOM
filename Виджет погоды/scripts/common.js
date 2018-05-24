a819351875b94cb567baebc3cedd43b3ab55b967
hackerman1@bk.ru <abduzeedo2@mail.ru>
2018-05-24 10:37:21 +0300

    widget weather

---
+++ Виджет погоды/scripts/common.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const request = new XMLHttpRequest();
+request.addEventListener('load', onLoad);
+request.open('GET', 'https://netology-fbb-store-api.herokuapp.com/weather');
+request.send();
+
+function onLoad() {
+  const response = JSON.parse(request.responseText);
+  setData(response);
+};
