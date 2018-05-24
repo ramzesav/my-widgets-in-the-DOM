92269dadbe2a2cb48976ca5cb3eee1c6bb396d5f
hackerman1@bk.ru <abduzeedo2@mail.ru>
2018-05-24 10:28:00 +0300

    window modal

---
+++ Модальное окно/scripts/modal-ui.js
@@ -0,0 +1,12 @@
+'use strict';
+
+function initModalUi() {
+    const triggers = document.querySelectorAll('.trigger');
+    for (let trigger of triggers) {
+        trigger.addEventListener('click', (event) => {
+            event.preventDefault();
+            document.querySelector('.modal-wrapper').classList.toggle('open');
+            document.querySelector('.page-wrapper').classList.toggle('blur');
+        });
+    }
+}
---
+++ Модальное окно/scripts/ui.js
@@ -0,0 +1,4 @@
+'use strict';
+// Инициализация функционала модального окна
+document.addEventListener('DOMContentLoaded', initModalUi);
+
